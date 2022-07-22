a21a5cb1801d07a940b76ed2e5ca865fda85adad
Markos <markosmk@me.com>
2022-07-22 00:23:41 -0300

    update: optimization in calls

--- src/app/country/services/country.service.ts
+++ src/app/country/services/country.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Country } from '../interfaces/country.interface';
@@ -8,19 +8,23 @@ import { Country } from '../interfaces/country.interface';
 })
 export class CountryService {
   private apiUrl: string = 'https://restcountries.com/v3.1';
+
+  get httpParams() {
+    return new HttpParams().set('fields', 'name,flags,capital,population,cca2');
+  }
   constructor(private http: HttpClient) {}
 
   getCountryByName(term: string): Observable<Country[]> {
     const url = `${this.apiUrl}/name/${term}`;
-    return this.http.get<Country[]>(url);
+    return this.http.get<Country[]>(url, { params: this.httpParams });
   }
   getCountryByCapital(term: string): Observable<Country[]> {
     const url = `${this.apiUrl}/capital/${term}`;
-    return this.http.get<Country[]>(url);
+    return this.http.get<Country[]>(url, { params: this.httpParams });
   }
   getCountryByRegion(term: string): Observable<Country[]> {
     const url = `${this.apiUrl}/region/${term}`;
-    return this.http.get<Country[]>(url);
+    return this.http.get<Country[]>(url, { params: this.httpParams });
   }
   getCountryByCode(code: string): Observable<Country[]> {
     const url = `${this.apiUrl}/alpha/${code}`;
